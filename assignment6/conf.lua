e88ad2d8deb07a2197d6009f067bc5b0bea62e77
kes360 <107447862+kes360@users.noreply.github.com>
2024-08-05 10:00:54 -0400

    Add files via upload

---
+++ assignment6/conf.lua
@@ -0,0 +1,7 @@
+-- this enables simple print statements to log to lovec console during gameplay
+-- from https://love2d.org/forums/viewtopic.php?t=79579
+
+function love.conf(t)
+	t.console = true
+end
+
